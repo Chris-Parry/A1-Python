a0657885a11908ae1c9bb8744a8c5507485abd90
csg524 <csg524@york.ac.uk>
2023-05-16 21:20:29 +0100

    added plotly plots

--- modules/cvs_tools.py
+++ modules/cvs_tools.py
@@ -1,6 +1,8 @@
 import glob
 
 import pandas as pd
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
 
 
 def convert_xls_to_csv() -> None:
@@ -22,3 +24,34 @@ def drop_empty_csv_rows() -> None:
         headers = df.columns.tolist()
 
         df.to_csv(csv, index=False, header=headers)
+
+
+def plotly_plot_csvs() -> None:
+    csv_files = glob.glob("data/Attension Data/*.csv")
+
+    # Create subplot with as many rows as files
+    fig = make_subplots(rows=len(csv_files), cols=1)
+
+    for i, file in enumerate(csv_files):
+        data = pd.read_csv(file)
+
+        fig.add_trace(
+            go.Scatter(
+                x=data["Time [s]"],
+                y=data["CA mean [°]"],
+                mode="lines",
+                name=f"Data from {file}",
+            ),
+            row=i + 1,
+            col=1,
+        )
+
+    fig.update_layout(
+        height=400 * len(csv_files),
+        width=800,
+        title_text="Subplots from multiple CSV files",
+    )
+    fig.show()
+
+
+plotly_plot_csvs()
