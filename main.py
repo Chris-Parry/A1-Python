89e1d39d3dd9c5ccbbcfba9a402ab84087544d8f
csg524 <csg524@york.ac.uk>
2023-05-19 16:13:31 +0100

    main.py better, power_deposition not working

--- main.py
+++ main.py
@@ -1,41 +1,53 @@
+import glob
+
 from modules.fit_data import fit_data
 from modules.load_data import load_data
-from modules.plot_data import plot_wave
+
+# from modules.plot_data import plot_wave
 from modules.save_data import save_data
 
 
 def main() -> None:
-    filepath = "data/Oscilloscope Data/scope_a1_1.csv"
-    x_data, y_wave1, y_wave2 = load_data(filepath)
-    initial_guesses_wave1: list[float] = [
-        0.3,  # Amplitude
-        13.56e6,  # Frequency
-        0,  # Phase shift
-        0,  # Vertical shift
-    ]
-    initial_guesses_wave2: list[float] = [
-        0.5,  # Amplitude
-        13.56e6,  # Frequency
-        0,  # Phase shift
-        0,  # Vertical shift
-    ]
-    params_wave1, params_wave2, phase_shift, phase_shift_degrees = fit_data(
-        x_data, y_wave1, y_wave2, initial_guesses_wave1, initial_guesses_wave2
-    )
-    difference_in_degrees = 90 - phase_shift_degrees
-    print(f"Phase shift (radians): {phase_shift:.3f}")
-    print(f"Phase shift (degrees): {phase_shift_degrees:.1f}")
-    print(f"Difference: {difference_in_degrees:.2f}")
-    plot_wave(
-        x_data,
-        y_wave1,
-        y_wave2,
-        params_wave1,
-        params_wave2,
-        filepath,
-        phase_shift_degrees,
-    )
-    save_data(filepath, phase_shift, phase_shift_degrees, difference_in_degrees)
+    filepath = glob.glob("data/Oscilloscope Data/scope_a1_pw2_*[0-9].csv")
+    filepath.sort()
+    differences = []
+    phase_shifts = []
+    for file in filepath:
+        x_data, y_wave1, y_wave2 = load_data(file)
+        initial_guesses_wave1: list[float] = [
+            0.3,  # Amplitude
+            13.56e6,  # Frequency
+            0,  # Phase shift
+            0,  # Vertical shift
+        ]
+        initial_guesses_wave2: list[float] = [
+            0.5,  # Amplitude
+            13.56e6,  # Frequency
+            0,  # Phase shift
+            0,  # Vertical shift
+        ]
+        params_wave1, params_wave2, phase_shift, phase_shift_degrees = fit_data(
+            x_data, y_wave1, y_wave2, initial_guesses_wave1, initial_guesses_wave2
+        )
+        difference_in_degrees = 90 - phase_shift_degrees
+        print(f"Phase shift (radians): {phase_shift:.3f}")
+        print(f"Phase shift (degrees): {phase_shift_degrees:.1f}")
+        print(f"Difference: {difference_in_degrees:.2f}")
+        differences.append(difference_in_degrees)
+        phase_shifts.append(phase_shift_degrees)
+        # plot_wave(
+        #     x_data,
+        #     y_wave1,
+        #     y_wave2,
+        #     params_wave1,
+        #     params_wave2,
+        #     file,
+        #     phase_shift_degrees,
+        # )
+        save_data(file, phase_shift, phase_shift_degrees, difference_in_degrees)
+    avg_difference = sum(differences) / len(differences)
+    print(f"Average phase shift: {sum(phase_shifts) / len(phase_shifts):.3f}")
+    print(f"Average difference: {avg_difference:.2f}")
 
 
 if __name__ == "__main__":
---
+++ modules/power_deposition.py
@@ -0,0 +1,35 @@
+import glob
+
+import numpy as np
+import pandas as pd
+
+# Constants
+Rt = 50  # Ohms
+Rm = 4.7  # Ohms
+
+# Get all files that match the pattern
+filepath = glob.glob("data/Oscilloscope Data/scope_a1_pw2_*[0-9].csv")
+
+for file in filepath:
+    # Read the CSV file, skipping the first two rows
+    df = pd.read_csv(file, skiprows=2, header=None)
+
+    # Rename the columns
+    df.columns = ["Time", "Voltage1", "Voltage2"]
+
+    # Convert the voltage to current
+    df["Current"] = (df["Voltage2"] * (Rm + Rt)) / (Rm * Rt)
+
+    # Convert from mV to mA
+    df["Current"] = df["Current"] / 1000
+
+    # Calculate the power
+    df["Power"] = df["Voltage1"] * df["Current"]
+
+    # Since the voltage and current are sinusoidal, their phase difference will affect the power
+    # Assuming that the phase difference is zero for now, as it's not specified in the question
+    theta = np.radians(3.935)
+    df["Power"] = df["Power"] * np.cos(theta)
+
+    # Save the dataframe back to the CSV
+    df.to_csv("data/Oscilloscope Data/new_" + file, index=False)
